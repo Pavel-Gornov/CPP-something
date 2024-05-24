e5c3b7c8ce90b23085c7e8fc8ff16206edefc0bf
Pavel-Gornov <77741438+Pavel-Gornov@users.noreply.github.com>
2024-05-24 22:51:45 +0400

    Gtest
    Описания не будет.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "gtest/gtest.h"
 #include <iostream>
 #include <vector>
 
@@ -8,14 +9,23 @@ std::vector<int> task(std::vector<int> v, int n) {
     return v;
 }
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    std::vector<int> v = {0, 2, 4};
+TEST(TestGroupName, Subtest_1) {
+    std::vector<int> v = { 0, 2, 4 };
     int n = 1;
     std::vector<int> v2 = task(v, n);
-    if (v2 == std::vector<int>{1, 3, 5}) {
-        std::cout << "Тестирование пройдено!";
-    }
-    return 0;
+    v = std::vector<int>{ 1, 3, 5 };
+    ASSERT_EQ(v2, v);
 }
 
+TEST(TestGroupName, Subtest_2) {
+    std::vector<int> v = {-1, 0, 3, 5, 9};
+    std::vector<int> v2 = task(v, 10);
+    v2 = task(v2, -10);
+    ASSERT_EQ(v2, v);
+}
+
+int main(int argc, char** argv){
+    setlocale(LC_ALL, "Russian");
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
